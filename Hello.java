bacd1aaabf4f8325e58fb9d36bf5b2d829e32bb6
Marathe Dipak <marathedipak831@gmail.com>
2026-06-28 15:23:56 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,6 +1,6 @@
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("Hello java");
         int sum = 10 + 5;
         System.out.print(sum);
 
